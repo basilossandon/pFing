bcee64aab2fe9ba665be087ee3d886d1e768d9e6
basil <basilossandon@gmail.com>
2018-09-11 01:27:43 -0300

    Implementacion Publicar Idea Terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Article } from './article/article.model';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {Idea} from './idea';
 
-
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,29 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'Alaya Platform';
   articles: Article[];  
-  constructor(){
+  idea = new Idea("nombre","psw",0);
+
+  constructor(private http: HttpClient){
+
+     this.get();
      this.articles = [
       new Article('Sistema distribuido que detecta posibles accidentes de tránsito mediante la geolocalización, conectado a través de un servicio.', 'http://Posteado por Juan Pablo Martinez hace 5 horas atrás. Comentarios Reportar', 3),];
   }
+
+  get(){
+    this.getUs().subscribe(data =>{
+      for (var i = data.length - 1; i >= 0; i--) {
+        this.articles.push(new Article(data[i].title, data[i].text, 0));      }
+    });
+  }
+
+  getUs() :Observable<Idea[]> {
+    return this.http.get<Idea[]>("http://localhost:1313/ideas");
+  }
+
   addArticle(title: HTMLInputElement, text: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and text: ${text.value}`);
     this.articles.push(new Article(title.value, text.value, 0));
@@ -27,5 +45,4 @@ export class AppComponent {
   sortedArticles(): Article[] {
     return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
-
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -12,27 +12,27 @@ import { Observable } from 'rxjs';
 })
 export class FormularioComponent implements OnInit {
 
-
-
-  idea = new Idea(null,null, 0);
+  idea = new Idea(null, null, 0);
 
   constructor(private http: HttpClient) { 
   }
+  
   ngOnInit() {
   }
-  submit(title: HTMLInputElement, text: HTMLInputElement): Idea {
-    console.log(this.idea.title);
+
+  submit(title: HTMLInputElement, text: HTMLInputElement){
     this.idea.title = title.value;
     this.idea.text = text.value;
-
+    console.log(this.idea.title);
     this.http.post("http://localhost:1313/ideas",this.idea)
     .subscribe(response => {
       console.log(response);
     });
-    return this.idea;
    }
+
    nameChange(e){
     console.log("change");
     console.log(e.target.value);
   }
 }
+
